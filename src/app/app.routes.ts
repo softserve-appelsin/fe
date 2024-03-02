80659ddb60425c5a1731ee23dacff1f964a8727f
Mark Bulavin <cloudlet@Marks-MacBook-Pro.local>
2024-03-02 15:17:28 +0200

    added routes to register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'register', component: RegisterComponent}
+];
