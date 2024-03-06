015ca98045cd9d928316278c8f11069c06fa3f0d
Mark Bulavin <cloudlet@Marks-MacBook-Pro.local>
2024-03-06 21:54:28 +0200

    added button to open modal window + styles

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { MatDialog, MatDialogModule } from '@angular/material/dialog'
+import { RegisterComponent } from '../register/register.component';
+ 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(private dialogRef: MatDialog) {}
+
+  openRegistrationModal() {
+    this.dialogRef.open(RegisterComponent);
+  }
 
 }
