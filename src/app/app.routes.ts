b44053ff63c2d713926c6c8d1be170f7a57f4a01
Mark Bulavin <cloudlet@Marks-MacBook-Pro.local>
2024-03-12 20:05:11 +0200

    updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 
 import { RegisterComponent } from './components/register/register.component';
 import { MainComponent } from './components/main/main.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 export const routes: Routes = [
-    { path: '', component: MainComponent },
+    { path: '', component: AppComponent},
+    { path: 'dashboard', component: MainComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent }
 ];
