1d89b640a0a187cda95ac4809c0fe0d010550295
Mark Bulavin <cloudlet@Marks-MacBook-Pro.local>
2024-03-13 23:24:21 +0200

    added slider into dashboard page

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { SliderComponent } from '../slider/slider.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, SliderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
