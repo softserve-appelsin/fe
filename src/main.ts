33f3c3235297155c68f36ac9d05bedf613497144
Mark Bulavin <cloudlet@Marks-MacBook-Pro.local>
2024-03-17 01:11:19 +0200

    created slider and moved logo to top

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { register as registerSwiperElement } from 'swiper/element/bundle'
+
+registerSwiperElement();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
