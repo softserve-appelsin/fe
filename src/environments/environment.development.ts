aec4a2ff219de966222684503e6f0cff169cfb09
Mark Bulavin <cloudlet@Marks-MacBook-Pro.local>
2024-03-24 13:33:55 +0200

    added env for stage, dev and prod

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://api.appelsin.stage.dnull.systems'
+};
---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    baseUrl: 'http://0.0.0.0:8000/'
+};
